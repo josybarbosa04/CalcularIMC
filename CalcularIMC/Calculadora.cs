951f567dbf56413b1981767c6a6618edf8c384e1
josybarbosa04 <josenilda_silva123@outlook.com>
2025-01-02 14:04:49 -0300

    Adicionar arquivos de projeto.

---
+++ CalcularIMC/Calculadora.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularIMC
+{
+    public class Calculadora
+    {
+        private double peso;
+        private double altura;
+        private double imc;
+        public String classificacao;
+
+        public double Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+
+        public double Imc
+        {
+            get { return imc; }
+            set { imc = value; }
+        }
+
+        public String Classificacao
+        {
+            get { return classificacao; }
+            set { classificacao = value; }
+        }
+
+        private void CalculoIMC()
+        {
+            double altQuadrado = (Altura * Altura);
+            Imc = Peso / altQuadrado;
+        }
+
+        private void ClassificaObesidade()
+        {
+            if (Imc <= 18.5)
+            {
+                Classificacao = "Você está abaixo do peso!";
+            } else if (Imc > 18.5 && Imc < 24.99)
+            {
+                Classificacao = "Você está no peso ideal!";
+            } else if (Imc > 24.99 && Imc < 29.99)
+            {
+                Classificacao = "Você está acima do peso!";
+            } else if (Imc > 29.99)
+            {
+                Classificacao = "Você está obeso!!!\nA obesidade é uma doença. Procure um profissional!";
+            }
+        }
+
+    }
+}
---
+++ CalcularIMC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calculadora = new Calculadora();
+            Console.WriteLine("*CALCULADORA DE IMC*");
+
+            Console.WriteLine("Digite seu peso: ");
+            calculadora.Peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(calculadora.Peso);
+
+            Console.WriteLine("Digite sua altura: ");
+            calculadora.Altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(calculadora.Altura);
+
+            Console.WriteLine("O seu IMC é:" + calculadora.Imc);
+            Console.WriteLine(calculadora.Classificacao);
+
+            Console.ReadKey();
+        }
+    }
+}
